9b4afe20025577a618429430d3656b027891f908
singsai <nick.tang@gmail.com>
2013-01-23 16:41:40 -0500

    Revert

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 WellnessProto::Application.initialize!
-ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| 
-  msg = instance.error_message 
-
-  if html_tag =~ /<(input|textarea|select)[>]+class=/
-    class_attribute = html_tag =~ /class=['"]/ 
-    html_tag.insert(class_attribute + 7, "error ") 
-  elsif html_tag =~ /<(input|textarea|select)/
-    first_whitespace = html_tag =~ /\s/ 
-    html_tag[first_whitespace] = " class='error' "
-  end 
-
-  html_tag
-}
