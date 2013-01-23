0a3afbfe0e3454b513ad0be340dedbb0b1662f49
singsai <nick.tang@gmail.com>
2013-01-23 10:33:46 -0500

    Testing mailer.

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -1,10 +1,10 @@
 #require 'pry'
 
 class MembershipMailer < ActionMailer::Base
-  default :from => "bahh@bah.com"
+  default :from => "nick.tang@gmail.com"
   
   def reminder_email(week, email)
-    mail(:to => email, :bcc => "bahh@bah.com", :subject => "You are being reminded", 
+    mail(:to => email, :bcc => "nick.tang@gmail.com", :subject => "You are being reminded", 
     :content_type => "text/html", :body => "Your weight for Week ##{week} is empty. Please fill it.")
   end
 end
