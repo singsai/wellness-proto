9361b2d262be5f9633ed4a08d56345aff93c6291
singsai <nick.tang@gmail.com>
2013-01-23 11:50:54 -0500

    Edit email address From

--- app/mailers/membership_mailer.rb
+++ app/mailers/membership_mailer.rb
@@ -1,15 +1,15 @@
 #require 'pry'
 
 class MembershipMailer < ActionMailer::Base
-  default :from => "nick.tang@gmail.com"
+  default :from => "ntang@holmanauto.com"
   
   def reminder_email(week, email)
-    mail(:to => email, :bcc => "nick.tang@gmail.com", :subject => "You are being reminded", 
+    mail(:to => email, :bcc => "ntang@holmanauto.com", :subject => "You are being reminded", 
     :content_type => "text/html", :body => "Your weight for Week ##{week} is empty. Please fill it.")
   end
   
   def registration_email(email, shib)
-    mail(:to => email, :bcc => "nick.tang@gmail.com", :subject => "You have just been registered", 
+    mail(:to => email, :bcc => "ntang@holmanauto.com", :subject => "You have just been registered", 
     :content_type => "text/html", :body => "You can log in with your email and secret word: #{shib}")
   end
 end
