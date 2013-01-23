8c0bb683838aa2cf257e0dc4621a0a1d7d1fa7bf
singsai <nick.tang@gmail.com>
2013-01-23 09:41:33 -0500

    E

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,10 +14,10 @@ WellnessProto::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  config.assets.initialize_on_precompile = false
+  #config.assets.initialize_on_precompile = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
