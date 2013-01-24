97bfb7854244c2c8d47171ae05c36001aa03bd3b
singsai <nick.tang@gmail.com>
2013-01-24 16:36:53 -0500

    THis better not break anything.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ WellnessProto::Application.configure do
   #config.assets.initialize_on_precompile = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
